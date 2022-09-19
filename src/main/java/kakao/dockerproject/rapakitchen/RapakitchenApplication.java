a90eb56d134fff89c5fd4223d2e6a63f9027a652
guswl197 <guswl197@ajou.ac.kr>
2022-09-19 14:16:35 +0900

    init commit

--- src/main/java/kakao/dockerproject/rapakitchen/RapakitchenApplication.java
+++ src/main/java/kakao/dockerproject/rapakitchen/RapakitchenApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RapakitchenApplication {
-	public static void main(String[] args) {
+
+		public static void main(String[] args) {
 		SpringApplication.run(RapakitchenApplication.class, args);
 	}
 
