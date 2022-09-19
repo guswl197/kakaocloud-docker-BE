ab5a1fa2314f8c7da207a55196a31dbb6a98d3c3
guswl197 <guswl197@ajou.ac.kr>
2022-09-19 13:56:24 +0900

    init commit

---
+++ src/main/java/kakao/dockerproject/rapakitchen/RapakitchenApplication.java
@@ -0,0 +1,13 @@
+package kakao.dockerproject.rapakitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapakitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapakitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kakao/dockerproject/rapakitchen/RapakitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package kakao.dockerproject.rapakitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapakitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
